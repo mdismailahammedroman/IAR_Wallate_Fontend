84cd786e3c01e8fc50eee627e8844ee65b75f52a
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-08-21 23:39:59 +0600

    set up shadcn done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello Iar digital wallet</h1>
+      <Button/>
+    </>
+  )
+}
+
+export default App
